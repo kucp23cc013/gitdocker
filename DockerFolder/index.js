f0cc7aada1163beb97e8d94ba5b49b1099bf47d3
kucp23cc013 <141497956+kucp23cc013@users.noreply.github.com>
2023-08-10 19:11:27 +0700

    Update index.js

--- DockerFolder/index.js
+++ DockerFolder/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/user', (req, res) => {
-    res.send({ "id": 11, "name": "Chandan" });
+    res.send({ "id": 11, "name": "KUCP23CC013" });
 });
 
 app.listen(5000, () => {
